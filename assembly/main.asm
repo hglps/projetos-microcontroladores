58cf4c0accb34a5e10ec83fa4c49d672dd18ed02
Lucas Massa <lmm@ic.ufal.br>
2021-12-16 02:51:45 -0300

    sem interrupcoes

--- assembly/main.asm
+++ assembly/main.asm
@@ -1,30 +1,21 @@
 ; Projeto Assembly de Microcontroladores
 ; Grupo: Hiago Lopes, Luana Ferreira e Lucas Massa
 
-.def andar=r16
+.def andar_atual=r16
 .def buzzer=r17
 .def led=r18
 .def open=r19
 .def temp = r20
+.def botoes_internos = r21
+.def botoes_externos = r22
+.def andar_internos = r23
+.def andar_externos = r24
 
 ; definindo vetor de interrupções
 .cseg
 jmp RESET
 
-.org $006
-jmp pcint0_handle
-
 .org $034
-
-pcint0_handle:
-	push r21
-	in r21, SREG
-	push r21
-	ldi r31, $AA
-	pop r21
-	out SREG, r21
-	pop r21
-	reti
 
 RESET: 
 ; inicializar pilha
@@ -34,21 +25,132 @@ ldi temp, low(RAMEND)
 out SPL, temp
 
 ;criando o estado inicial: porta aberta com elevador parado e buzzer desligado
-ldi andar, 0
+ldi andar_atual, 0
 ldi buzzer, 0
 ldi led, 1
 ldi open, 1
 
-ldi temp,0x00
+; 4 ultimos bits da PORTB para servir de botoes internos
+ldi temp,0b11110000
 out DDRB,temp
 
-; habilitando interrupcoes nos pinos da porta B
-ldi temp,0b111
-sts PCICR, temp
-ldi temp, $FF
-sts PCMSK0, temp
+; ultimo bit de PORTC para servir de botao fecha porta
+ldi temp,0b11111110
+out DDRC,temp
 
-sei
+; 4 ultimos bits da PORTD para servir de botoes externos
+ldi temp,0b11110000
+out DDRD,temp
 
-MAIN_LOOP: ; definindo o loop principal
-	jmp MAIN_LOOP
+jmp PORTA_ABERTA
+
+BIT_SIGNIFICATIVO_INTERNOS:
+	mov temp, botoes_internos
+	andi temp, 0b1000
+	tst temp
+	brne int_3
+
+	mov temp, botoes_internos
+	andi temp, 0b0100
+	tst temp
+	brne int_2
+
+	mov temp, botoes_internos
+	andi temp, 0b0010
+	tst temp
+	brne int_1
+
+	mov temp, botoes_internos
+	andi temp, 0b0001
+	tst temp
+	brne int_0
+
+	int_3:
+		ldi andar_internos, 3
+		ret
+	int_2:
+		ldi andar_internos, 2
+		ret
+	int_1:
+		ldi andar_internos, 1
+		ret
+	int_0:
+		ldi andar_internos, 0
+		ret
+
+BIT_SIGNIFICATIVO_EXTERNOS:
+	mov temp, botoes_externos
+	andi temp, 0b1000
+	tst temp
+	brne ext_3
+
+	mov temp, botoes_externos
+	andi temp, 0b0100
+	tst temp
+	brne ext_2
+
+	mov temp, botoes_externos
+	andi temp, 0b0010
+	tst temp
+	brne ext_1
+
+	mov temp, botoes_externos
+	andi temp, 0b0001
+	tst temp
+	brne ext_0
+
+	ext_3:
+		ldi andar_externos, 3
+		ret
+	ext_2:
+		ldi andar_externos, 2
+		ret
+	ext_1:
+		ldi andar_externos, 1
+		ret
+	ext_0:
+		ldi andar_externos, 0
+		ret
+
+	
+PORTA_FECHADA:
+	in botoes_internos, PINB
+	nop
+	in botoes_externos, PIND
+	nop
+
+	call BIT_SIGNIFICATIVO_INTERNOS
+	call BIT_SIGNIFICATIVO_EXTERNOS
+
+	cp andar_atual, andar_externos
+	brlo SUBIR_ANDAR
+	cp andar_atual, andar_internos
+	brlo SUBIR_ANDAR
+
+	cp andar_atual, andar_externos
+	breq PORTA_ABERTA
+	cp andar_atual, andar_internos
+	breq PORTA_ABERTA
+
+	jmp DESCER_ANDAR
+	
+PORTA_ABERTA:
+	in botoes_internos, PINB
+	nop
+	in botoes_externos, PIND
+	nop
+	in open, PINC
+	nop
+
+	mov temp open
+	tst temp
+	breq PORTA_ABERTA
+	jmp PORTA_FECHADA
+
+SUBIR_ANDAR:
+	inc andar_atual
+	jmp PORTA_FECHADA
+
+DESCER_ANDAR:
+	dec andar_atual
+	jmp PORTA_FECHADA
