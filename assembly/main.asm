ed380f3f251dd14fa372e78156d1fc664e221fd0
hglps <44714747+hglps@users.noreply.github.com>
2021-12-18 00:47:16 -0300

    buzzer e led por pinos, IR de timer e delay

--- assembly/main.asm
+++ assembly/main.asm
@@ -13,9 +13,11 @@
 
 ; definindo vetor de interrupcoes
 .cseg
+
 jmp RESET
 jmp FECHAR_PORTA
-
+.org OC1Aaddr
+jmp TIMER_5us_IR
 .org $034
 
 RESET:
@@ -34,6 +36,37 @@ sts EICRA, temp
 ldi temp, (1 << INT0)
 out EIMSK, temp
 
+;faz setup para uso do timer de 5us
+#define CLOCK 8.0e6 ;velodidade do clock: 8MHz
+#define DELAY 5.0e-6 ;5 microssegundos
+.equ PRESCALE = 0b001 ;/1 prescale
+.equ PRESCALE_DIV = 1
+.equ WGM = 0b0100 ;Waveform generation mode: CTC
+;garante que nao passe de 65535
+.equ TOP = int(0.5 + ((CLOCK/PRESCALE_DIV)*DELAY))
+.if TOP > 65535
+.error "TOP is out of range"
+.endif
+
+;On MEGA series, write high byte of 16-bit timer registers first
+;valor a ser calculado
+ldi temp, high(TOP) ;initialize compare value (TOP)
+sts OCR1AH, temp
+ldi temp, low(TOP)
+sts OCR1AL, temp
+ldi temp, ((WGM&0b11) << WGM10) ;lower 2 bits of WGM
+sts TCCR1A, temp
+;upper 2 bits of WGM and clock select
+ldi temp, ((WGM>> 2) << WGM12)|(PRESCALE << CS10)
+sts TCCR1B, temp ;start counter
+;TCCR1B: 000 01       001
+;            WGM CTC; prescale 
+
+;setup da IR de timer
+lds temp, TIMSK1
+sbr temp, 1 <<OCIE1A
+sts TIMSK1, temp
+
 ;criando o estado inicial: porta aberta com elevador parado e buzzer desligado
 ldi andar_atual, 0
 ldi buzzer, 0
@@ -49,6 +82,10 @@ out DDRB,temp
 ; 4 ultimos bits da PORTC para servir de botoes externos
 ldi temp,0b11110000
 out DDRC,temp
+
+; 2 ultimos bits da PORTD para servir de led(1) e buzzer(0), respectivamente
+ldi temp,0b00000011
+out DDRD,temp
 
 jmp PORTA_ABERTA
 
@@ -162,7 +199,7 @@ ANDAR_ATINGIDO:
 		ret
 		
 
-ABRIR_PORTA:	
+ABRIR_PORTA:
 	ldi closed, 0
 	call ANDAR_ATINGIDO
 	call BIT_SIGNIFICATIVO_INTERNOS
@@ -170,10 +207,59 @@ ABRIR_PORTA:
 	jmp PORTA_ABERTA
 
 FECHAR_PORTA:
+	push temp
+	in temp, SREG
+	push temp
+
 	ldi closed, 1
+	;in temp, TCCR1B
+	; ultimos 3 bits definem prescale: 001 = /1 prescale
+	;                                  000 = timer stopped
+	;dec temp ; 001 => 000
+	ldi temp, ((WGM>> 2) << WGM12)|(0 << CS10)
+	sts TCCR1B, temp ;start counter
+	;sts TCCR1B, temp ;start counter
+
+	pop temp
+	out SREG, temp
+	pop temp
 	reti
+
+TIMER_5us_IR:
+	push temp
+	in temp, SREG
+	push temp
+	
+	; verifica se PORTD[0] está ligado == 1
+	;in temp, PORTD
+	;nop
+	tst buzzer ; se buzzer == 0, ligar_buzzer
+	breq ligar_buzzer
+
+	; desligando buzzer: ao desligar buzzer, porta fecha e led apaga tambem
+	ldi temp, 0
+	out PORTD, temp
+	nop
+	
+	jmp TIMER_5us_IR_END
+
+	ligar_buzzer:
+		ldi buzzer, 1
+		ldi temp, 0b11
+		out PORTD, temp
+		nop
+
+	TIMER_5us_IR_END:
+	
+		pop temp
+		out SREG, temp
+		pop temp
+		reti
+	
 	
 PORTA_FECHADA:
+	cli
+	ldi led, 0 ; porta fechada
 	in botoes_internos, PINB
 	nop
 	in botoes_externos, PINC
@@ -195,7 +281,21 @@ PORTA_FECHADA:
 	jmp DESCER_ANDAR
 	
 PORTA_ABERTA:
+	; ativa led
+	ldi led, 1 ; porta aberta
+	;in temp, PORTD
+	nop
+	
+	mov temp, buzzer
+	or temp, 0b00000010
+	out PORTD, temp
+	nop
+
+	; ativa interrupcao de timer de 5us
+	ldi temp, ((WGM>> 2) << WGM12)|(PRESCALE << CS10)
+	sts TCCR1B, temp ;start counter
 	sei ;;;;;;;;;;;;;;;;;;;;;;;;;
+
 	in botoes_internos, PINB
 	nop
 	in botoes_externos, PINC
@@ -207,9 +307,37 @@ PORTA_ABERTA:
 	jmp PORTA_FECHADA
 
 SUBIR_ANDAR:
+	cli
+	call TIMER_5us
+	call TIMER_5us
 	inc andar_atual
 	jmp PORTA_FECHADA
 
 DESCER_ANDAR:
+	cli
+	call TIMER_5us
+	call TIMER_5us
 	dec andar_atual
 	jmp PORTA_FECHADA
+
+TIMER_5us:
+	in temp, TIFR1 ;request status from timers
+	andi temp, 1<<OCF1A ;isolate only timer 1's match
+	; 0b1 << OCF1A = 0b1 << 1 = 0b00000010
+	; andi --> 1 (OCF1A � um)	--> overflow
+	; andi --> 0 (OCF1A � zero)	--> contando
+	breq skipoverflow ;skip overflow handler
+	;match handler - done once every DELAY seconds
+	ldi temp, 1<<OCF1A ;write a 1 to clear the flag
+	out TIFR1, temp
+	jmp TIMER_5us_END
+
+	;overflow event code goes here
+	
+	skipoverflow:
+		;main application processing goes here
+		nop
+		rjmp TIMER_5us
+
+	TIMER_5us_END:
+		ret
